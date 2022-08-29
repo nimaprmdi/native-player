a64db2213085b88872fbaf7b3be1e1dc85799489
nimaprmdi <nimaprmdi@gmail.com>
2022-08-29 19:12:23 +0430

    Fix minor issues

--- src/components/common/cards/Card.tsx
+++ src/components/common/cards/Card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface CardProps {
     id: string;
     image: string;
@@ -6,10 +8,19 @@ interface CardProps {
 }
 
 const Card = ({ id, image, title }: CardProps) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
     return (
         <a href="#" className="c-card__item-link w-full text-h5 font-bold hover:text-accent">
             <div className="c-card__item w-full">
-                <img className="c-card__image w-full rounded" src={image} alt="album pic" />
+                <img
+                    className={`c-card__image w-full rounded h-72 ${isLoaded ? "block" : "hidden"}`}
+                    src={image}
+                    onLoad={() => setIsLoaded(true)}
+                    alt="album pic"
+                />
+
+                {!isLoaded && <div className="c-card__loader w-full h-72 relative"></div>}
             </div>
         </a>
     );
--- src/components/common/cards/CardRibbon.tsx
+++ src/components/common/cards/CardRibbon.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { textCutter } from "../../../utils/helpers";
 
 interface CardRibbonProps {
@@ -8,10 +9,19 @@ interface CardRibbonProps {
 }
 
 const CardRibbon = ({ id, image, readMore = true, title }: CardRibbonProps) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
     return (
         <a href="#" className="c-card__item-link w-full text-h5 font-bold hover:text-accent">
             <div className="c-card__item w-full">
-                <img className="c-card__image w-full rounded-t h-72 object-cover" src={image} alt="album pic" />
+                <img
+                    className={`c-card__image w-full rounded-t h-72 object-cover ${isLoaded ? "block" : "hidden"}`}
+                    src={image}
+                    onLoad={() => setIsLoaded(true)}
+                    alt="album pic"
+                />
+
+                {!isLoaded && <div className="c-card__loader w-full h-72 relative"></div>}
 
                 <div className="c-card__item-cta w-full text-center pt-4 pb-4 bg-gray-800 text-white rounded-b">
                     {readMore ? "Listen Now" : textCutter(title!, 20)}
